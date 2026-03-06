d008ca9d796899d16b21b3700d6d777dd68e3fdf
dgTr3 <Edugta31@gmail.com>
2026-03-05 21:18:15 -0600

    completo

--- Do-While19/Program.cs
+++ Do-While19/Program.cs
@@ -1,1 +1,10 @@
 ﻿Console.WriteLine("Tabla de multiplicar con el número que el usuario ingrese");
+Console.WriteLine("Ingrese un número");
+int numero=int.Parse(Console.ReadLine());
+int i = 1;
+do
+{
+    Console.WriteLine(numero + " x " + i + "=" + (numero * i));
+    i++;
+}
+while (i <= 10);
